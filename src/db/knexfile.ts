303f629beab95ea0eb23571f3ba9b8fe1f1e6fa9
sanjeevbhu22 <sanjeevkumar.bhumca22@gmail.com>
2023-12-05 23:18:12 +0530

    migration added

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -5,38 +5,6 @@
  */
 module.exports = {
 
-  // development: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './dev.sqlite3'
-  //   }
-  // },
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
-  // development: {
-  //   client: 'postgresql',
-    // connection: {
-    //   host : 'localhost',
-    //   port : 3306,
-    //   user : 'root',
-    //   password : 'sanjeev8084',
-    //   database : 'dailyupdates'
-    // },
     development: {
       client: 'postgresql',
       connection: {
