ca25cd4ede4064e5f076fa4dc0f94b1a1457c76f
sanjeevbhu22 <sanjeevkumar.bhumca22@gmail.com>
2023-12-23 11:44:54 +0530

    modified migration file

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -1,15 +1,15 @@
-// Update with your config settings.
-
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+require('dotenv').config();
+
 module.exports = {
 
     development: {
-      client: 'postgresql',
+      client: process.env.client,
       connection: {
         host: process.env.host, 
-        user:  process.env.uses,
+        user:  process.env.user,
         password: process.env.password,
         database: process.env.database,
         port: process.env.port,
@@ -19,7 +19,7 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migration'
+      tableName: process.env.tableName
     }
   }
 
